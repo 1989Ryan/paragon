207efda89d44a0e99128df38017b35d9fc6f3ab6
1989Ryan <ziruiz@comp.nus.edu.sg>
2023-02-07 21:45:02 +0800

    update

--- scripts/pretrain_model.py
+++ scripts/pretrain_model.py
@@ -9,3 +9,11 @@ if not os.path.isdir('./tmp'):
     os.mkdir('./tmp')
 os.system(f'tar -xv {output} -C ./tmp')
 os.system(f'rm {output}')
+
+
+url = 'https://drive.google.com/uc?export=download&id=1li3Y1M_aSblB77HZ_d4qX3wumDJNMYdl'
+output = 'maskrcnn_pretrain.tar.gz'
+
+gdown.download(url, output, quiet=False)
+os.system(f'tar -xv {output} -C ./object_detector/maskrcnn/')
+os.system(f'rm {output}')
